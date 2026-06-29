286a9bc778df598e74461b0bf6eb4dbc603abedf
lowolfmun <nachitocalero@gmail.com>
2026-06-29 16:26:38 -0600

    Modulo 1.2: Se añadio imports a ServicioAtencion

---
+++ MediQueue/src/main/java/servicios/ServicioAtencion.java
@@ -0,0 +1,23 @@
+package servicios;
+
+import estructuras.Lista;
+import estructuras.Nodo;
+import estructuras.Pila;
+import modelos.Consulta;
+import modelos.EntradaHistorial;
+import modelos.Tiquete;
+import persistencia.Json;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class ServicioAtencion {
+
+}
